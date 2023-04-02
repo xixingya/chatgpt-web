7602b2f430580a8df4ff88434cb813c19c26662e
xixingya <2679431923@qq.com>
2023-04-02 11:46:51 +0800

    change desc

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -46,7 +46,7 @@ onMounted(() => {
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
+          此项目基于开源版
           <a
             class="text-blue-600 dark:text-blue-500"
             href="https://github.com/Chanzhaoyu/chatgpt-web"
@@ -57,7 +57,7 @@ onMounted(() => {
           ，免费且基于 MIT 协议，没有任何形式的付费行为！
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
+					本服务只作为服务端部署，由于使用人数过多，不定期修改验证码，请加群获取
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'xixingya',
+      description: 'Open AI <a href="https://openai.com" class="text-blue-500" target="_blank" >OpenAI</a>',
     },
   }
 }
