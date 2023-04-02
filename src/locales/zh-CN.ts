6a301be4c4186a1a5e9c68d2874c14be486177cc
xixingya <2679431923@qq.com>
2023-04-02 11:22:34 +0800

    add qq group

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -25,7 +25,7 @@ export default {
     success: '操作成功',
     failed: '操作失败',
     verify: '验证',
-    unauthorizedTips: '未经授权，请先进行验证。',
+    unauthorizedTips: '加入qq群573960837获取验证码。',
   },
   chat: {
     newChatButton: '新建聊天',
