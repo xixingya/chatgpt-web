b7bf358d3a2bf5ce1d3b7d0d890be5022ea806b1
xixingya <2679431923@qq.com>
2023-04-08 19:15:34 +0800

    feat: add redis

--- service/src/db/redisconf.ts
+++ service/src/db/redisconf.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv'
 
 dotenv.config()
 const REDIS_CONF = {
-  port: 6379,
+  port: process.env.REDIS_PORT,
   host: process.env.REDIS_URL,
   password: process.env.REDIS_PASSWORD,
 }
